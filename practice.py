15fb0b8c63f9e63d0f05178538cac17fb49c3712
LeeEugene1 <uwm1004@gmail.com>
2021-03-17 21:04:03 +0900

    slice

--- practice.py
+++ practice.py
@@ -28,3 +28,9 @@ station = '신도림'
 print(station,'행 열차가 들어오고 있습니다.')
 station = '인천공항'
 print(station,'행 열차가 들어오고 있습니다.')
+
+#문자열은 ''과 "", '''가있다
+sentence = """여러줄이
+가능한
+문자열"""
+print(sentence)
---
+++ ramdom.py
@@ -0,0 +1,24 @@
+from random import *
+
+# print(int(random() * 10)) #1~10미만의 랜덤생성
+# print(int(random() * 10) + 1) #1~10이하의 랜덤생성
+
+#로또는 1~45
+# print(int(random() * 45) + 1)#1~45이하의 랜덤생성
+# print(int(random() * 45) + 1)#1~45이하의 랜덤생성
+# print(int(random() * 45) + 1)#1~45이하의 랜덤생성
+# print(int(random() * 45) + 1)#1~45이하의 랜덤생성
+# print(int(random() * 45) + 1)#1~45이하의 랜덤생성
+# print(int(random() * 45) + 1)#1~45이하의 랜덤생성
+
+# print(randrange(1,46)) #1~46미만의 랜덤생성
+# print(randrange(1,46)) #1~46미만의 랜덤생성
+# print(randrange(1,46)) #1~46미만의 랜덤생성
+# print(randrange(1,46)) #1~46미만의 랜덤생성
+# print(randrange(1,46)) #1~46미만의 랜덤생성
+# print(randrange(1,46)) #1~46미만의 랜덤생성
+
+# print(randint(1,45)) #1~45이하의 랜덤생성
+
+random_date = randint(4,28)
+print('오프라인 스터디 모임 날짜는 매월', random_date, '일로 선정되었습니다.')
