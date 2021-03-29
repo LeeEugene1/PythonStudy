0ebe5229fba1c9fd277a0a4614f605f2b54b1942
LeeEugene1 <uwm1004@gmail.com>
2021-03-29 14:21:01 +0900

    calculator

---
+++ calculator.py
@@ -0,0 +1,14 @@
+a = int(input('첫번째숫자를 입력하라'))
+b = int(input('두번째숫자를 입력하라'))
+
+result = a + b
+print(a,'+',b,'=',result)
+
+result = a - b
+print(a,'-',b,'=',result)
+
+result = a * b
+print(a,'*',b,'=',result)
+
+result = a/b
+print(a,'/','b','=',result)
