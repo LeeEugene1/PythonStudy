3c2ed9cbcb02b383861aed69ce18e45125586ba3
LeeEugene1 <uwm1004@gmail.com>
2021-04-03 15:11:22 +0900

    진수변환

---
+++ binOctHex.py
@@ -0,0 +1,20 @@
+#파이썬 진수변환 함수
+#bin() : 2진수, oct() : 8진수, hex() : 16진수
+
+sel = int(input('입력 진수 결정(16/10/8/2)'))
+num = input('값 입력:')
+
+num10 = 0
+if sel == 16:
+    num10 = int(num, 16)
+elif sel == 10:
+    num10 = int(num, 10)
+elif sel == 8:
+    num10 = int(num,8)
+elif sel == 2:
+    num10 = int(num,2)
+
+print('16진수->',hex(num10))
+print('10진수->',num10)
+print('8진수->',oct(num10))
+print('2진수->',bin(num10))
