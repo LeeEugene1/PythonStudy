c546d5113b98d3da60196b68ccadbc4b455e3d69
LeeEugene1 <uwm1004@gmail.com>
2021-04-03 15:11:54 +0900

    진수변환2

---
+++ shift연산자.py
@@ -0,0 +1,16 @@
+# a = 100
+# result = 0
+# i = 0
+
+# for i in range(1,5):
+#     result = a << i
+#     # print('%d << %d = %d' % (a, i, result))
+#     print(f'{a} << {i} = {result}')
+
+# for i in range(1,5):
+#     result = a >> i
+#     # print('%d >> %d = %d' % (a, i, result))
+#     print(f'{a} >> {i} = {result}')
+
+a = 12345
+print(~a + 1)
